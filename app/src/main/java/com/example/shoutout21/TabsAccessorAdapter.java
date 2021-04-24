cdd84755646174cefb489b8ad5e8620cc2809cd0
Edward Kong <dejingk@gmail.com>
2021-04-24 07:47:41 -0400

    Guys, try this Shout Out 2.1

---
+++ app/src/main/java/com/example/shoutout21/TabsAccessorAdapter.java
@@ -0,0 +1,56 @@
+package com.example.shoutout21;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabsAccessorAdapter extends FragmentPagerAdapter {
+    public TabsAccessorAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+            case 1:
+                GroupsFragment groupsFragment = new GroupsFragment();
+                return groupsFragment;
+            case 2:
+                ContactsFragment contactsFragment = new ContactsFragment();
+                return contactsFragment;
+            case 3:
+                RequestFragment requestFragment = new RequestFragment();
+                return requestFragment;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "Chat";
+            case 1:
+                return "Group";
+            case 2:
+                return "Contact";
+            case 3:
+                return "Request";
+            default:
+                return null;
+        }
+    }
+}
